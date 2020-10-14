ce2877fb821b78c5d0e5fd0b204573befa993794
Quinn Thiessen <Quinn.Github@gmail.com>
2020-10-13 23:24:48 -0500

    syntax error, oops maybe test code before you commit

--- src/prefixStuff.py
+++ src/prefixStuff.py
@@ -11,7 +11,7 @@ def get_prefix(guild):
 def change_prefix(ctx, new_prefix: str):
     with open('./src/prefixes.json', 'r') as f:
         prefixes = json.load(f)
-    logging.debug(f'{ctx.author} (user id {ctx.author.id}) changed the prefix of {ctx.guild} (guild id {ctx.guild.id}) from '{prefixes[str(guild.id)]}')
+    logging.debug(f'{ctx.author} (user id {ctx.author.id}) changed the prefix of {ctx.guild} (guild id {ctx.guild.id}) from \'{prefixes[str(guild.id)]}\' to \'{new_prefix}\'')
     prefixes[str(ctx.guild.id)] = new_prefix
     with open('./src/prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
